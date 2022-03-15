26fcbc26188156005f0fa7d89ced8c9258dfd428
gt <gurkanturkarslan@hotmail.com>
2022-03-16 02:32:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
     <BrowserRouter>
@@ -12,7 +10,6 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
