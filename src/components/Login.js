2bc1ba35cdc41db795565265df5cf87fbec2f0e9
gt <gurkanturkarslan@hotmail.com>
2022-03-18 02:10:40 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -59,7 +59,7 @@ const Login = (props) => {
 
     return (
         <div className={"col-md-12"}>
-            <div className={"card card-container deck"}>
+            <div className={"card card-container"}>
                 <img
                     src={"//ssl.gstatic.com/accounts/ui/avatar_2x.png"}
                     alt={"profile-img"}
