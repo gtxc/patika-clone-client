1a58b92f2200a9833603eeb1474825428111f023
gt <gurkanturkarslan@hotmail.com>
2022-03-18 12:46:26 +0300

    Add navbar define routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,109 @@
+import React, {useState, useEffect} from "react";
+import {Link, Route, Routes} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import AuthService from "./services/auth.service";
 import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import BoardUser from "./components/BoardUser";
+import BoardModerator from "./components/BoardModerator";
+import BoardAdmin from "./components/BoardAdmin";
 
-function App() {
+const App = () => {
+    const [showModeratorBoard, setShowModeratorBoard] = useState(false);
+    const [showAdminBoard, setShowAdminBoard] = useState(false);
+    const [currentUser, setCurrentUser] = useState(undefined);
+
+    useEffect(() => {
+        const user = AuthService.getCurrentUser();
+        if (user) {
+            setCurrentUser(user);
+            setShowModeratorBoard(user.roles.includes("MODERATOR"));
+            setShowAdminBoard(user.roles.includes("DEV"));
+        }
+    }, []);
+
+    const logOut = () => {
+        AuthService.logOut();
+    };
+
     return (
-        <div className="App">
-            <Login/>
+        <div>
+            <nav className={"navbar navbar-expand navbar-dark bg-dark"}>
+                <Link to={"/"} className={"navbar-bran"}>
+                    GTXC
+                </Link>
+                <div className={"navbar-nav mr-auto"}>
+                    <li className={"nav-item"}>
+                        <Link to={"/home"} className={"nav-link"}>
+                            Home
+                        </Link>
+                    </li>
+                    {showModeratorBoard && (
+                        <li className={"nav-item"}>
+                            <Link to={"/mod"} className={"nav-link"}>
+                                Moderator Board
+                            </Link>
+                        </li>
+                    )}
+                    {showAdminBoard && (
+                        <li className={"nav-item"}>
+                            <Link to={"/dev"} className={"nav-lnk"}>
+                                DEV Board
+                            </Link>
+                        </li>
+                    )}
+                    {currentUser && (
+                        <li className={"nav-item"}>
+                            <Link to={"/user"} className={"nav-link"}>
+                                User
+                            </Link>
+                        </li>
+                    )}
+                </div>
+                {currentUser ? (
+                    <div className={"navbar-nav ml-auto"}>
+                        <li className={"nav-item"}>
+                            <Link to={"/profile"} className={"nav-link"}>
+                                {currentUser.username}
+                            </Link>
+                        </li>
+                        <li className={"nav-item"}>
+                            <a href={"/login"} className={"nav-link"} onClick={logOut}>
+                                LogOut
+                            </a>
+                        </li>
+                    </div>
+                ) : (
+                    <div className={"nav-bar ml-auto"}>
+                        <li className={"nav-item"}>
+                            <Link to={"/login"} className={"nav-link"}>
+                                Login
+                            </Link>
+                        </li>
+                        <li className={"nav-item"}>
+                            <Link to={"/register"} className={"nav-link"}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </div>
+                )}
+            </nav>
+            <div className={"container mt-3"}>
+                <Routes>
+                    <Route exact path={["/", "/home"]} element={<Home />} />
+                    <Route exact path={"/login"} element={<Login />} />
+                    <Route exact path={"/register"} element={<Register />} />
+                    <Route exact path={"/profile"} element={<Profile />} />
+                    <Route path={"/user"} element={<BoardUser />} />
+                    <Route path={"/mod"} element={<BoardModerator />} />
+                    <Route path={"/dev"} element={<BoardAdmin />} />
+                </Routes>
+            </div>
         </div>
     );
-}
+};
 
 export default App;
