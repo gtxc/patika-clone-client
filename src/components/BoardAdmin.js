ade8249643d824422347f5837c0d175f6811bc66
gt <gurkanturkarslan@hotmail.com>
2022-03-18 11:32:50 +0300

    Add BoardAdmin.js BoardModerator.js BoardUser.js personal pages

---
+++ src/components/BoardAdmin.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from "react";
+import UserService from "../services/user.service";
+
+const BoardAdmin = () => {
+    const [content, setContent] = useState("");
+    useEffect(() => {
+        UserService.getAdminBoard().then(
+            (response) => {
+                setContent(response.data);
+            },
+            (error) => {
+                const _content =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+                setContent(_content);
+            }
+        );
+    }, []);
+
+    return (
+        <div className={"container"}>
+            <div className={"jumbotron"}>
+                <h3>{content}</h3>
+            </div>
+        </div>
+    );
+};
+
+export default BoardAdmin;
